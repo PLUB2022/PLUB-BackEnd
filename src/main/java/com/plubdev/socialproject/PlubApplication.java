cb076b9edd2e3c39a6900963ad10b3b52459d384
sectionr0 <sectionr0@gmail.com>
2022-04-03 18:14:01 +0900

    Initial commit
    Spring setting

---
+++ src/main/java/com/plubdev/socialproject/PlubApplication.java
@@ -0,0 +1,13 @@
+package com.plubdev.socialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlubApplication.class, args);
+	}
+
+}
